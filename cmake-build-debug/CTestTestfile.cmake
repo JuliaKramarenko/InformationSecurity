66270afa35dcb2d07185420c934ebfef114f8355
JuliaKramarenko <kramarenko.julia@unicyb.kiev.ua>
2020-10-07 23:57:08 +0300

    .gitignore fix

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/juliy/CLionProjects/aes-kalyna
-# Build directory: C:/Users/juliy/CLionProjects/aes-kalyna/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("library")
-subdirs("executable")
-subdirs("tests")
--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/juliy/CLionProjects/aes-kalyna/tests
-# Build directory: C:/Users/juliy/CLionProjects/aes-kalyna/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(aes_kalyna_tst "C:/Users/juliy/CLionProjects/aes-kalyna/cmake-build-debug/tests/aes_kalyna_tst.exe")
-set_tests_properties(aes_kalyna_tst PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/juliy/CLionProjects/aes-kalyna/tests/CMakeLists.txt;9;add_test;C:/Users/juliy/CLionProjects/aes-kalyna/tests/CMakeLists.txt;0;")
